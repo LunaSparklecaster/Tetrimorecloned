06543c71441d953d939d60856524590811e52601
lunasparklecaster <lunastarcaster@gmail.com>
2016-12-29 22:39:06 -0800

    Added more notes, early shape names

--- game_area.py
+++ game_area.py
@@ -22,8 +22,15 @@ def rotate_offsets(offsets, rotation):
 def coords_to_check(type, center_location, rotation):
     coordinate_list = []
     # todo: dictionary-ify
+    # T shape is shaped like a T with the middle post down, center might be on post in this version or on middle
+    # I piece/stick is safe to shift depending on how we handle collision
     shape_lookup = {'square': [[0, 0], [0, 1], [1, 0], [1, 1]],
-                    'stick': [[-1, 0], [0, 0], [1, 0], [2, 0]]}
+                    'stick': [[-1, 0], [0, 0], [1, 0], [2, 0]],
+                    'Tee':[],
+                    'S_shap':[],
+                    'Z_shap':[],
+                    'L_shap':{},
+                    'J_shap':(),}
     offsets = shape_lookup[type]
 
     offsets = rotate_offsets(offsets, rotation)
@@ -33,7 +40,9 @@ def coords_to_check(type, center_location, rotation):
 
 
 # testing the function we just built
-game_space = [[0 for a in range(22)] for b in range(10)]
+# 10 wide 20 high playing field, outer 2 walls on sides are wall checks and top 2 are death zone
+# TODO pieces should spawn under dead zone. check top of screen failstate.
+game_space = [[0 for a in range(22)] for b in range(14)]
 coords = coords_to_check('square', (4, 12), 2)
 free = minimal_check_free(coord_list=coords, game_space=game_space)
 assert free is True
@@ -41,6 +50,7 @@ assert free is True
 
 # todo: add "walls" by initializing the outer edges of the data structure to non-zero values
 # this will let our code check the existing areas w/o modification, and will always prevent going through walls.
+#so data set 2 wider for wall checks since the I piece is the only one to occupy 2 spaces away from center.
 # todo: kill zone, line clear
 # todo: generate an object at random
 # todo: user input
